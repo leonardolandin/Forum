64d46130a1cfff94c28fb65e1490fc8335c2a507
Ajudando <ajudando.ajudinha07@gmail.com>
2019-09-16 22:21:59 -0300

    Modal singup

--- script/main.js
+++ script/main.js
@@ -3,11 +3,14 @@ vm = this;
 vm.modal = function(tipo) {
     if (tipo == 'login') {
     document.getElementById('auth-login').style.display='flex'
+    document.getElementById('auth-login').style.background='rgba(0,0,0, 0.4)'
     }
     if (tipo == 'cadastro') {
-        document.getElementById('auth-login').style.display='flex'
+        document.getElementById('auth-cadastro').style.display='flex'
+        document.getElementById('auth-cadastro').style.background='rgba(0,0,0, 0.4)'
     }
     if (tipo == 'exit') {
         document.getElementById('auth-login').style.display='none'
+        document.getElementById('auth-cadastro').style.display='none'
     }    
 }
