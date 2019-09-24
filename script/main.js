05cbef77e09898f81dfe182ffd7f8d306f06b012
Ajudando <ajudando.ajudinha07@gmail.com>
2019-09-23 21:28:56 -0300

    Recuperar senha

--- script/main.js
+++ script/main.js
@@ -2,8 +2,8 @@ vm = this;
 
 vm.modal = function(tipo) {
     if (tipo == 'login') {
-    document.getElementById('auth-login').style.display='flex'
-    document.getElementById('auth-login').style.background='rgba(0,0,0, 0.4)'
+        document.getElementById('auth-login').style.display='flex'
+        document.getElementById('auth-login').style.background='rgba(0,0,0, 0.4)'
     }
     if (tipo == 'cadastro') {
         document.getElementById('auth-cadastro').style.display='flex'
@@ -12,5 +12,21 @@ vm.modal = function(tipo) {
     if (tipo == 'exit') {
         document.getElementById('auth-login').style.display='none'
         document.getElementById('auth-cadastro').style.display='none'
-    }    
+    }
+    if (tipo == 'exit-esqueceu') {
+        document.getElementById('modal-esqueceu').style.display='none'
+    }
+    if (tipo == 'esqueceu') {
+        document.getElementById('modal-esqueceu').style.display='flex'
+    } 
 }
+
+//Elementos com ações desabilitadas
+vm.actionsPrevent = ["esqueceu", "exit-esqueceu"]
+
+document.getElementById('esqueceu').addEventListener("click", function(event){
+    event.preventDefault()
+});
+document.getElementById('exit-esqueceu').addEventListener("click", function(event){
+    event.preventDefault()
+});
